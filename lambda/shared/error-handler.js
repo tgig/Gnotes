c8836ae06250c881e0bafc1e32a24a6c47f5d24f
tgig <tgiggy@swingbyswing.com>
2015-12-13 22:12:12 -0800

    Error handler pushes error data to SNS

--- lambda/shared/error-handler.js
+++ lambda/shared/error-handler.js
@@ -1,4 +1,18 @@
-function LogError(error) {
-  console.log('Error: ' + error);
-}
-module.exports.LogError = LogError;
+var AWS = require('aws-sdk');
+
+exports.LogError = function(error) {
+
+  var sns = new AWS.SNS();
+  sns.publish({
+     TopicArn: 'arn:aws:sns:us-east-1:420261107226:Dropbox_Evernote_Error_Email',
+     Subject: 'Error ' + arguments.callee.caller.name,
+     Message: error
+  }, function(err, data) {
+      if (err) {
+          throw('Error pushing error messge to SNS: ' + err);
+      }
+      console.log('Error message pushed to SNS');
+      console.log(data);
+      throw(new Error('Error message pushed to SNS: ' + errorSubject + '\n' + errorMessage));
+  });
+};
