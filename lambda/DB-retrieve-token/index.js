902af73243fec255de90afccb3671ed7f66c3a93
tgig <tgiggy@swingbyswing.com>
2016-01-04 09:12:02 -0800

    retrieve dropbox file cursor and store in db

--- lambda/DB-retrieve-token/index.js
+++ lambda/DB-retrieve-token/index.js
@@ -129,6 +129,46 @@ function getDropboxEmail(dropboxUserId, dropboxAuthToken, callback) {
 
 }
 
+function getDropboxCursor(dropboxUserId, dropboxAuthToken, callback) {
+  var postUrl = 'https://api.dropboxapi.com/2/files/list_folder/get_latest_cursor';
+  var postData = {
+    headers: {
+      'Authorization': 'Bearer ' + dropboxAuthToken
+    },
+    json: {
+      'path': '',
+      'recursive': false,
+      'include_media_info': false,
+      'include_deleted': false
+    }
+  };
+
+  request.post(postUrl, postData, function (error, response, body) {
+
+    if (error) {
+      return ErrorHandler.LogError('Error in getDropboxCursor: ' + error);
+    }
+    else if (response.statusCode == 400) {
+      return ErrorHandler.LogError('Error in getDropboxCursor. Response body: ' + response.body);
+    }
+
+    var data = JSON.parse(body);
+    var cursor = data.cursor;
+
+    console.log('getDropboxCursor data: ' + data);
+
+    saveDropboxCursor(dropboxUserId, cursor, function(err, cursorData) {
+      if (err) {
+        return ErrorHandler.LogError('Error when saving cursor: ' + err);
+      }
+
+      callback();
+    });
+
+
+  });
+}
+
 function getEvernoteOAuthLink(callback) {
   var client = new Evernote.Client ({
     consumerKey: process.env.evernote_consumer_key,
@@ -138,7 +178,7 @@ function getEvernoteOAuthLink(callback) {
 
   client.getRequestToken(callbackUrl, function(err, oauthToken, oauthSecret, results){
     if(err) {
-      throw(new Error('Error in getEvernoteOAuthLink: ' + err));
+      return ErrorHandler.LogError('Error in getEvernoteOAuthLink: ' + err);
     }
     else {
       enData = {
@@ -172,6 +212,32 @@ function saveDropboxToken(dropboxUserId, dropboxToken, callback) {
     ExpressionAttributeValues: {
       ":dropboxToken": {
        "S": dropboxToken
+      }
+    },
+    ReturnValues: "ALL_NEW"
+  };
+
+
+  dynamodb.updateItem(params, function(err, data) {
+    if (err)
+      callback(err);
+    else
+      callback(null, data);
+  });
+}
+
+function saveDropboxCursor(dropboxUserId, dropboxFileCursor, callback) {
+  var params = {
+    TableName: "DropboxEvernoteUser",
+    Key: {
+      "DropboxUserId": {
+        "N": dropboxUserId.toString()
+      }
+    },
+    UpdateExpression: "SET DropboxFileCursor = :dropboxFileCursor",
+    ExpressionAttributeValues: {
+      ":dropboxFileCursor": {
+       "S": dropboxFileCursor
       }
     },
     ReturnValues: "ALL_NEW"
@@ -229,24 +295,32 @@ function main(code, callback) {
     dropboxAuthToken = dropboxUserIdAndToken.dropboxToken;
 
     getDropboxEmail(dropboxUserId, dropboxAuthToken, function(err, dropboxEmailData) {
+      if (err) {
+        return ErrorHandler.LogError('Error in getDropboxEmail: ' + err);
+      }
 
-      getEvernoteOAuthLink(function(err, evernoteData) {
+      getDropboxCursor(dropboxUserId, dropboxAuthToken, function(err, dropboxCursorData) {
         if (err) {
-          return ErrorHandler.LogError('Error in getEvernoteOAuthLink callback: ' + err);
+          return ErrorHandler.LogError('Error in getDropboxCursor: ' + err);
         }
 
-        returnData = {
-          "dropboxUserId": dropboxUserId,
-          "evernoteOAuthToken": evernoteData.oauthToken,
-          "evernoteOAuthSecret": evernoteData.oauthSecret,
-          "evernoteAuthorizeUrl": evernoteData.authorizeUrl
-        }
-        callback(null, returnData);
+        getEvernoteOAuthLink(function(err, evernoteData) {
+          if (err) {
+            return ErrorHandler.LogError('Error in getEvernoteOAuthLink callback: ' + err);
+          }
+
+          returnData = {
+            "dropboxUserId": dropboxUserId,
+            "evernoteOAuthToken": evernoteData.oauthToken,
+            "evernoteOAuthSecret": evernoteData.oauthSecret,
+            "evernoteAuthorizeUrl": evernoteData.authorizeUrl
+          }
+          callback(null, returnData);
+        });
+
       });
 
     });
-
-
 
   });
 }
